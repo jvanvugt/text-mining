c6a01fa1b7734e7d901395c0bcda921653c76784
Joris van Vugt <jorisvan.vugt@student.ru.nl>
2016-09-21 16:23:11 +0200

    Fix finding dates

--- week4/convert_dates.py
+++ week4/convert_dates.py
@@ -16,17 +16,18 @@ MONTHS = [
     ]
 MONTHS_REGEX = '|'.join(MONTHS)
 
-# A dictionary mapping regex patterns to lambdas to convert them to ISO 8601 dates
-PATTERNS = {
-    '(%s) (\d\d?),? (\d\d\d\d)' % MONTHS_REGEX: 
-        lambda r: '%s-%02d-%02d' % (r.group(3), int(MONTHS.index(r.group(1)) + 1), int(r.group(2))),
-    '(%s),? (\d\d\d\d)' % MONTHS_REGEX:
-        lambda r: '%s-%02d' % (r.group(2), MONTHS.index(r.group(1)) + 1),
-    '\d\d\d\d-\d\d?-\d\d?': 
-        lambda r: r.group(0),
-    '\d\d\d\d': 
-        lambda r: r.group(0),
-}
+# An (ordered) list of tupples mapping regex patterns 
+# to lambdas to convert them to ISO 8601 dates
+PATTERNS = [
+    ('(%s) (\d\d?),? (\d\d\d\d)' % MONTHS_REGEX,
+        lambda r: '%s-%02d-%02d' % (r.group(3), int(MONTHS.index(r.group(1)) + 1), int(r.group(2)))),
+    ('(%s),? (\d\d\d\d)' % MONTHS_REGEX,
+        lambda r: '%s-%02d' % (r.group(2), MONTHS.index(r.group(1)) + 1)),
+    ('\d\d\d\d-\d\d?-\d\d?',
+        lambda r: r.group(0)),
+    ('\d\d\d\d', 
+        lambda r: r.group(0)),
+]
     
 
 
@@ -37,13 +38,23 @@ def convert_dates(filename):
             # Save the original line, so we can remove already
             # matched dates so we don't match them again'
             original_line = line
+            done = False
 
-            for pattern, convert in PATTERNS.items():
-                date = re.search(pattern, line)
-                if date is not None:
-                    line = line.replace(date.group(0), '')
-                    iso_date = convert(date)
-                    print(original_line, iso_date, sep='\t')
+            # Keep trying all rules on the sentence until none
+            # matched
+            while True:
+                for pattern, convert in PATTERNS:
+                    found = False
+                    date = re.search(pattern, line)
+                    if date is not None:
+                        # Remove found date from the sentence
+                        line = line.replace(date.group(0), '')
+                        iso_date = convert(date)
+                        print(original_line, iso_date, sep='\t')
+                        found = True
+                        break
+                if not found:
+                    break
     os.remove(filename + '.out')
 
 if __name__ == '__main__':
