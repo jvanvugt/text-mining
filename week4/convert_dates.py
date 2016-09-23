cf124313fddba46b151a32ff9f4e725f895e5df0
Joris van Vugt <jorisvan.vugt@student.ru.nl>
2016-09-23 13:50:24 +0200

    Final adjustments

--- week4/convert_dates.py
+++ week4/convert_dates.py
@@ -19,12 +19,16 @@ MONTHS_REGEX = '|'.join(MONTHS)
 # An (ordered) list of tupples mapping regex patterns 
 # to lambdas to convert them to ISO 8601 dates
 PATTERNS = [
+    # E.g., January 5, 2016
     ('(%s) (\d\d?),? (\d\d\d\d)' % MONTHS_REGEX,
         lambda r: '%s-%02d-%02d' % (r.group(3), int(MONTHS.index(r.group(1)) + 1), int(r.group(2)))),
+    # E.g., January, 2016
     ('(%s),? (\d\d\d\d)' % MONTHS_REGEX,
         lambda r: '%s-%02d' % (r.group(2), MONTHS.index(r.group(1)) + 1)),
+    # E.g., 2016-01-05 (already ISO date)
     ('\d\d\d\d-\d\d?-\d\d?',
         lambda r: r.group(0)),
+    # E.g., 2016 (just a year)
     ('\d\d\d\d', 
         lambda r: r.group(0)),
 ]
@@ -33,11 +37,13 @@ PATTERNS = [
 
 def convert_dates(filename):
     nlp.clean(filename)
+    dates = []
     with open(filename + '.out', 'r') as file:
         for line in file:
             # Save the original line, so we can remove already
-            # matched dates so we don't match them again'
-            original_line = line
+            # matched dates so we don't match them again
+            # We also remove the newline at the end of the line
+            original_line = line[:-1]
             done = False
 
             # Keep trying all rules on the sentence until none
@@ -50,12 +56,16 @@ def convert_dates(filename):
                         # Remove found date from the sentence
                         line = line.replace(date.group(0), '')
                         iso_date = convert(date)
-                        print(original_line, iso_date, sep='\t')
+                        dates.append((iso_date, original_line))
                         found = True
                         break
                 if not found:
                     break
     os.remove(filename + '.out')
+    dates = sorted(dates, key=lambda d: d[0])
+    for date in dates:
+        print(' | '.join(date))
+
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
