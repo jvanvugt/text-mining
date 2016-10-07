9677c9bcfc47473da15e6e5424eafae257e83a90
Joris van Vugt <jorisvan.vugt@student.ru.nl>
2016-10-07 17:33:50 +0200

    Finish week 6

--- week6/classifier.py
+++ week6/classifier.py
@@ -5,11 +5,13 @@ Predict sentiment of tweets containing #Trump
 Author: Joris van Vugt
 """
 
+import re
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.metrics import precision_score, recall_score, f1_score
+from sklearn.metrics import classification_report, f1_score
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import KFold
@@ -20,7 +22,6 @@ def load_data(filename):
     Load the annotated data from a csv
     """
     data = pd.read_csv(filename, na_values='?')
-    print(data.Annotation.value_counts())
     return data
 
 def load_dictionary(filename):
@@ -30,22 +31,61 @@ def load_dictionary(filename):
     with open(filename) as f:
         return f.read().splitlines()
 
-def learn(data):
+pos_words = load_dictionary('positive_words.txt')
+pos_words.remove('trump')
+
+neg_words = load_dictionary('negative_words.txt')
+
+def maybe_replace(word, pos_token='<P>', neg_token='<N>'):
+    """
+    Return a token if the word is in the list of positive or negative words
+    """
+    for pos in pos_words:
+        if word == pos:
+            return pos_token
+
+    for neg in neg_words:
+        if word == neg:
+            return neg_token
+
+    return word
+
+def preprocess(tweet):
+    """
+    Preprocess a tweet to reduce vocabulary size
+    """
+    # Remove hashtags and mentions
+    tweet = tweet.replace('#', '').replace('@', '')
+    # Split CamelCase into Camel Case
+    tweet = re.sub('([a-z])([A-Z])', r'\1 \2', tweet)
+    # Make lowercase and split into words
+    tweet = tweet.lower().split()
+    # Map all positive words and negative words into their
+    # respective category
+    tweet = ' '.join([maybe_replace(word) for word in tweet])  
+    return tweet
+
+
+def learn(data, **kwargs):
     """
     Use machine learning to predict tweet sentiments
 
     The data is split into 10 folds.
     Each fold is predicted by a naive bayes classifer
     trained on the other 9 folds.
+
+    Return the predictions
     """
     k_fold = KFold(n_splits=10, shuffle=True)
     pipeline = Pipeline([
-        ('vectorizer', CountVectorizer(ngram_range=(7, 7), analyzer='char')),
+        ('vectorizer', CountVectorizer(**kwargs)),
         ('tfidf', TfidfTransformer()),
         ('classifier', MultinomialNB()),
-        ])
+    ])
 
-    X, y = data.Tweet, data.Annotation
+    X = data.Tweet.apply(preprocess)
+
+    y = data.Annotation
     predictions = np.empty(len(y))
     for train_idx, test_idx in k_fold.split(X, y):
         pipeline.fit(X.iloc[train_idx], y.iloc[train_idx])
@@ -64,6 +104,8 @@ def naive_wordcount(data, prior=True):
 
     If the number of positive and negative words are equal,
     the tweet is assigned the prior.
+
+    Return the predictions
     """
     pos_words = load_dictionary('positive_words.txt')
     neg_words = load_dictionary('negative_words.txt')
@@ -86,22 +128,41 @@ def naive_wordcount(data, prior=True):
 
     return predictions
 
-def classify(classifier=naive_wordcount, **kwargs):
+def classify(classifier=learn, **kwargs):
     """
     Classify tweets using a given classifier method and
     print performance metrics of the classifier
     
     kwargs are passed to the classifier method
+
+    Return the f1-score of the classifier
     """
     data = load_data('tweets.csv').dropna()
     data.Annotation = data.Annotation == 'P'
     predictions = classifier(data, **kwargs)
     ground_truth = data.Annotation.as_matrix()
+    data['Prediction'] = predictions.astype(np.bool)
+    data.to_csv('output.csv')
 
-    print('Precision:', precision_score(ground_truth, predictions))
-    print('Recall:', recall_score(ground_truth, predictions))
-    print('F1:', f1_score(ground_truth, predictions))
+    targets = ['Negative', 'Positive']
 
+    print(classification_report(
+        ground_truth, predictions, target_names=targets))
+    
+    return f1_score(ground_truth, predictions, average='macro')
+    
+
+def grid_search():
+    """
+    Exhaustively try all combinations of (hardcoded)
+    parameters
+
+    The performance of each combination is averaged over 20 runs 
+    """
+    for n_gram in zip(range(1, 8), range(1, 8)):
+        for analyzer in ('word', 'char', 'char_wb'):
+            mean_f1 = np.array([classify(learn, ngram_range=n_gram, analyzer=analyzer) for _ in range(20)]).mean()
+            print('n_gram: {}, analyzer: {} -> f1={}'.format(n_gram, analyzer, mean_f1))
 
 if __name__ == '__main__':
     classify(classifier=learn)
