c6ced953a590d572cbf255cda0e7d8ebe0c7bc0a
Joris van Vugt <jorisvan.vugt@student.ru.nl>
2016-11-14 21:19:21 +0100

    Add lemmatization with Frog

--- project/code/preprocess.py
+++ project/code/preprocess.py
@@ -12,6 +12,14 @@ import ntpath
 
 from unidecode import unidecode
 from tqdm import tqdm
+import frog
+
+def lemmatize(sentence, frogger):
+    """
+    Convert each word in a sentence to its lemma
+    """
+    lemmas = [token['lemma'] for token in frogger.process(sentence)]
+    return ' '.join(lemmas)
     
 
 def preprocess(input_folder, output_folder):
@@ -20,16 +28,22 @@ def preprocess(input_folder, output_folder):
     The cleaned files will be saved in the output folder
 
     Remove the XML-tags and clean the remaining raw text
-    to have one sentence per line
+    to have one sentence per line with lemmatized words
     """
     if not os.path.exists(output_folder):
         os.mkdir(output_folder)
-    files = glob.glob(input_folder + '/*.xml')
+
     tag_regex = re.compile('<[^>]*>')
     eos_regex = re.compile(r'(\w)\. ([A-Z])')
+    punctuation_regex = re.compile(r'[\.,:;/\(\)\[\]\'\"]')
+    frog_options = frog.FrogOptions(tok=False, morph=False, mwu=False, 
+                                    chunking=False, ner=False)
+    frogger = frog.Frog(frog_options, '/etc/frog/frog.cfg')
+
+    files = glob.glob(input_folder + '/*.xml')
     for file_name in tqdm(files):
         with open(file_name, 'r', encoding='utf-8') as file:
-            try: 
+            try:
                 text = unidecode(file.read())
                 # Remove all XML tags
                 text = tag_regex.sub('', text)
@@ -38,13 +52,17 @@ def preprocess(input_folder, output_folder):
                 lines = [line.strip() for line in lines]
                 # One sentence per line
                 lines = [eos_regex.sub('\\1.\n\\2', line) for line in lines]
-                # Remove empty lines
-                lines = [line for line in lines if line != '']
+                # Remove punctuation
+                lines = [punctuation_regex.sub('', line) for line in lines]
+                # Remove empty lines and make lower case
+                lines = [line.lower() for line in lines if line != '']
+                # Convert each word to its lemma
+                lemmas = [lemmatize(line, frogger) for line in lines]
                 # Change extension to .txt
                 outfile = ntpath.basename(file_name)[:-4] + '.txt'
                 out_name = os.path.join(output_folder, outfile)
                 with open(out_name, 'w', encoding='utf-8') as out:
-                    out.write('\n'.join(lines))
+                    out.write('\n'.join(lemmas))
             except UnicodeError:
                 print('Skipping {}, UnicodeError'.format(file_name))
 
@@ -53,5 +71,4 @@ if __name__ == '__main__':
     if len(sys.argv) != 3:
         print('Usage: {} input_folder output_folder'.format(__file__))
     else:
-        print(sys.argv)
         preprocess(sys.argv[1], sys.argv[2])
