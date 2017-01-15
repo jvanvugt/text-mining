d887ccc99032127473fd69b0da7733a78f5bc255
Joris van Vugt <jorisvan.vugt@student.ru.nl>
2017-01-15 03:36:32 +0100

    Do TSNE on subset of data

--- project/code/tsne.py
+++ project/code/tsne.py
@@ -5,10 +5,15 @@ import numpy as np
 from sklearn.manifold import TSNE
 
 
-def run(filename):
+def run(filename, ground_truth):
     with open(filename, 'rb') as file:
         model = pickle.load(file)
-    words = sorted(model.keys())
+
+    with open('D:/Data/justitiethesaurus_2015.xml.ths', 'rb') as file:
+        ground_truth = pickle.load(file)
+
+    relevant_words = set(sum([[word] + ground_truth[word] for word in ground_truth.keys()], []))
+    words = sorted(word.replace(' ', '_') for word in model.keys() if word in relevant_words)
     vectors = np.zeros((len(words), len(model[words[0]])), dtype=np.float32)
     for i, word in enumerate(words):
         vectors[i, :] = model[word]
@@ -18,7 +23,7 @@ def run(filename):
         pickle.dump(embeddings, file)
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print('Usage:\n python {} vectors'.format(__file__))
+    if len(sys.argv) != 3:
+        print('Usage:\n python {} vectors ground_truth'.format(__file__))
     else:
-        run(sys.argv[1])
+        run(sys.argv[1], sys.argv[2])
